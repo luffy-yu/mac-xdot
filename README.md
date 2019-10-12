5be638852f9bd18f9705b9a757c8894d05d9348b
luffy-yu <ylch_top@sina.com>
2019-10-12 13:51:56 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# xdot.py for Mac
+
+## Appearance
+
+![./appearance.png](./appearance.png)
+
+## Reference
+
+- https://github.com/jrfonseca/xdot.py.git
+
+- https://github.com/GNOME/adwaita-icon-theme.git
+
+
+## Fixed some errors like as follows
+
+- 'Error loading theme icon 'zoom-in' for stock:'
+
+## Usage
+
+### Run
+
+- python3 sample.py
+
+### Install
+
+- python3 setup.py install --install-scripts=/usr/local/var/pyenv/shims/
+
+- mac-xdot
---
+++ sample.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#
+# Copyright 2008 Jose Fonseca
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import gi
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+
+import xdot
+
+settings = Gtk.Settings.get_default()
+settings.set_property("gtk-theme-name", None)
+settings.set_property("gtk-application-prefer-dark-theme", False)  # if you want use dark theme, set second arg to True
+
+
+class MyDotWindow(xdot.DotWindow):
+
+    def __init__(self):
+        xdot.DotWindow.__init__(self)
+        self.dotwidget.connect('clicked', self.on_url_clicked)
+
+    def on_url_clicked(self, widget, url, event):
+        dialog = Gtk.MessageDialog(
+            parent=self,
+            buttons=Gtk.ButtonsType.OK,
+            message_format="%s clicked" % url)
+        dialog.connect('response', lambda dialog, response: dialog.destroy())
+        dialog.run()
+        return True
+
+
+dotcode = """
+digraph G {
+  Hello [URL="http://en.wikipedia.org/wiki/Hello"]
+  World [URL="http://en.wikipedia.org/wiki/World"]
+    Hello -> World
+}
+"""
+
+
+def main():
+    window = MyDotWindow()
+    # window.set_dotcode(dotcode)
+    window.connect('delete-event', Gtk.main_quit)
+    Gtk.main()
+
+
+if __name__ == '__main__':
+    main()
